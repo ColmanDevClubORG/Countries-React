d41531d88b1617abbc612be10a13bc2796d15672
hodaya123 <hodayak3@gmail.com>
2024-12-23 17:32:19 +0200

    add a json data to the page and implement search input

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 
-const Country = () => {
+export const Country = ({name ,flag , population , region , capital}) => {
   return (
     // TODO: Country component
-    <div>Country</div>
+    <div className="country scale-effect"> 
+    <img src={flag} className="country-flag"></img>
+    <div className="country-info">
+    <div className="country-brief">
+    <p>{name}</p>
+    <p>{population}</p>
+    <p>{region}</p>
+    <p>{capital}</p>
+    </div>
+    </div>
+    </div>
   )
 }
 
 export default Country
+
+
+// "name": "Åland Islands",
+// "flag": "https://flagcdn.com/w320/ax.png",
+// "population": 21225,
+// "region": "Europe",
+// "capital": "Mariehamn"
