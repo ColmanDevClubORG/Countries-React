c16d7fea90157075a6eddfba5cf6189aa0c0741b
tomergol15 <golod505@gmail.com>
2024-12-26 20:54:04 +0200

    adding country cards by import from the jsonData, filter it by region and by search, and add func of change to dark mode - react actions

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
-const Country = () => {
+const Country = ({cardData}) => {
   return (
-    // TODO: Country component
-    <div>Country</div>
+    <>
+    <a href="details.html" className="country scale-effect" data-country-name={cardData.name}>
+          <div className="country-flag">
+            <img src={cardData.flag}
+              alt={cardData.name} />
+          </div>
+          <div className="country-info">
+            <h2 className="country-title">{cardData.name}</h2>
+            <ul className="country-brief">
+              <li><strong>population: </strong>{cardData.population}</li>
+              <li><strong>Region: </strong>{cardData.region}</li>
+              <li><strong>capital: </strong>{cardData.capital}</li>
+            </ul>
+          </div>
+        </a>
+    </>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,73 @@
 import React from "react";
+import dataJson from "../assets/CountriesData.json"
+import { useState } from "react";
+import Country from "../components/Country"
+import Header from "../components/Header"
+import Filters from "../components/Filters"
+
+
 
 const Home = () => {
+    const [filteredArray, setfilteredArray] = useState(dataJson);
+    const [isDarkMode, setIsDarkMode] = useState(false); 
+    const filterByRegion = (region) => {
+        console.log(region);
+        
+        if(region==="All"){
+            setfilteredArray(dataJson);
+        }
+        else{
+            const filtered = dataJson.filter((Country)=>
+                Country.region.toLowerCase() === region.toLowerCase());
+            setfilteredArray(filtered)
+        }
+    };
+
+    const matchValue = (country,input) =>{
+        return country.name.toLowerCase().includes(input) ||
+            country.population.toString().includes(input) ||
+            country.region.toLowerCase().includes(input) ||
+            country.capital.toLowerCase().includes(input)
+    }
+
+    const searchInput = (event)=>{
+        console.log("enter");
+        console.log(event);
+        const ourInput = event.toLowerCase().trim();
+        const filteredArrayBySearch = dataJson.filter((country)=>{
+           return matchValue(country,ourInput);
+        })
+        setfilteredArray(filteredArrayBySearch);
+    }
+
+    const changeDarkMode = () => {
+        setIsDarkMode(!isDarkMode);
+
+        if (!isDarkMode) {
+            document.body.classList.add("dark-theme");
+        } else {
+            document.body.classList.remove("dark-theme");
+        }
+    };
+
+   
     return (
-        // TODO: Home page
-        // Render Country component (components/Country.jsx) for each country
-        // Take data from (assets/CountriesData.json)
-        <div>Home</div>
-    );
+        <>
+            <Header changeDarkMode={changeDarkMode} />
+            <Filters filterByRegionFunc={(region)=>filterByRegion(region)} filterBySearchFunc={(event)=>{searchInput(event.target.value)}} />
+            <main className="main">
+                <div className="container">
+                    <section className="countries-grid">
+                    {filteredArray.map((cardInfo,index)=>(
+                        <Country cardData={cardInfo} key={cardInfo.name} />
+                    ))}
+                    </section>
+                </div>
+            </main>
+
+        </>
+    )
 };
+
 
 export default Home;
