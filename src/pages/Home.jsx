64720a263a7e636fc5f0a49f2dcf729fc2a0e67b
Tal Sadan <talsadan0112@gmail.com>
2024-12-28 18:28:41 +0200

    added the header and search bar and made filter by region func

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,35 @@
 import { useState } from "react";
 import { Country } from "../components/Country";
 import data from "../assets/CountriesData.json";
+import Header from "../components/Header";
+import SearchBar from "../components/SearchBar";
 
 export const Home = () => {
-    const [countries, setcountries] = useState(data);
-    return (
-        <div className="main container">
-            <div className="countries-grid" >
-                {
-                    countries.map(
-                    (country) => {return <Country key={country.name} {...country}
-                     />
-                     })
-               }
-            </div>
+  const [countries, setCountries] = useState(data);
+
+  const filterByRegionFunc = (region) => {
+    const filteredCountries =
+      region === "All"
+        ? data
+        : data.filter((country) => country.region === region);
+    setCountries(filteredCountries);
+  };
+
+  return (
+    <>
+      <Header />
+      <SearchBar
+        filterByRegionFunc={filterByRegionFunc}
+      />
+      <div className="main container">
+        <div className="countries-grid">
+          {countries.map((country) => (
+            <Country key={country.name} {...country} />
+          ))}
         </div>
-    );
+      </div>
+    </>
+  );
 };
-export default Home
+
+export default Home;
