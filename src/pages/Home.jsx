5cbcd00b2a6c59d095197c8ff73d0e697ff2ede6
AdiAmar1 <165654012+AdiAmar1@users.noreply.github.com>
2025-01-01 12:57:38 +0200

    fetched data from api successfully

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,43 @@ import React from "react";
 import {Header} from "../components/Header";
 import { Filters } from "../components/Filters";
 import { Country } from "../components/Country";
-import datajson from "../assets/CountriesData.json";
+import { useState } from "react";
+import { useEffect } from "react";
 
 
 
 const Home = () => {
+
+    const [countries,setCountries]=useState([]);
+
+    useEffect(()=>{
+        const fetchCountries=async()=>{
+            try{
+                const response=await fetch("https://restcountries.com/v3.1/all");
+                const data= await response.json();
+
+                console.log("data fetched");
+
+                const filteredData=data.map((country)=>({
+                    name: country.name.common,
+                    population: country.population,
+                    region: country.region,
+                    capital: country.capital ? country.capital[0] :"N/A",
+                    flag: country.flags.svg,
+                }));
+
+                console.log("filtered data",filteredData);
+
+                setCountries(filteredData);
+            }catch (error){
+                console.error("Error fetching countries:", error);
+            }
+        };
+        fetchCountries();
+
+    },[]);
+
+
     return (
      
         <div>
@@ -22,8 +54,12 @@ const Home = () => {
             <div className="main">
                 <div className="container">
                     <div className="countries-grid">
-                        {datajson.map((cardD, index) => (
-                            <Country countryData={cardD} key={cardD.name}></Country>
+                        {countries.map((countryData) => (
+                            <Country
+                            countryData={countryData}
+                            key={countryData.name}    
+                            onClick={()=>openModal(countryData)}
+                            />
                         ))}
                     </div>
                 </div>
