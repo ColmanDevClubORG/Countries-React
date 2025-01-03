7643263fdecb1d43f035f367a1d1c5f5774cade1
OrYossef <165793124+OrYossef@users.noreply.github.com>
2025-01-03 21:14:41 +0200

    done API

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 
-const Country = () => {
+const Country = ({ name, flag, population, region, capital, onClick }) => {
   return (
-    // TODO: Country component
-    <div>Country</div>
-  )
-}
+    <div className='country' onClick={onClick}>
+      <div className="country-flag">
+        <img src={flag} alt={`Flag of ${name}`} />
+      </div>
+      <div className="country-info">
+        <h3 className="country-title">{name}</h3>
+        <ul className="country-brief">
+          <li>Population: {population.toLocaleString()}</li>
+          <li>Region: {region}</li>
+          <li>Capital: {capital}</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default Country
+export default Country;
